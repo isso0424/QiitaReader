f15c4fadcddffc64023623608d9757406c4bf947
isso0424 <isso04240129@gmail.com>
2019-10-16 22:08:13 +0900

    refactor #1

--- app/src/main/java/com/example/qiitaapp/MainActivity.kt
+++ app/src/main/java/com/example/qiitaapp/MainActivity.kt
@@ -4,62 +4,69 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.os.Handler
 import android.widget.*
 
 class MainActivity : AppCompatActivity(),Runnable{
-    lateinit var listView:ListView
-    lateinit var status:TextView
-    lateinit var task:GetData<AppCompatActivity>
-    var handler = Handler()
-    var links:Array<String?> = arrayOfNulls(30)
-    lateinit var daily: Button
-    lateinit var weekly:Button
-    lateinit var monthly:Button
+    private lateinit var listView:ListView
+    private lateinit var status:TextView
+    private lateinit var task:GetData<AppCompatActivity>
+    private var handler = Handler()
+    private var links:Array<String?> = arrayOfNulls(30)
+    private lateinit var daily: Button
+    private lateinit var weekly:Button
+    private lateinit var monthly:Button
+    private val res = resources
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         listView = findViewById(R.id.listView)
         status = findViewById(R.id.status)
-        task = GetData(listView, this, "daily", status)
-        status.setText("Loading")
+        task = GetData(listView, this, status)
+        GetData.scope = "daily"
+        status.text = res.getString(R.string.loading)
         task.execute(1)
-        daily = findViewById<Button>(R.id.daily)
-        weekly = findViewById<Button>(R.id.weekly)
-        monthly = findViewById<Button>(R.id.monthly)
+        daily = findViewById(R.id.daily)
+        weekly = findViewById(R.id.weekly)
+        monthly = findViewById(R.id.monthly)
         daily.setOnClickListener{
-            task = GetData(listView, this, "daily", status)
-            status.setText("Loading")
+            GetData.scope = "daily"
+            task = GetData(listView, this, status)
+            status.text = res.getString(R.string.loading)
             task.execute(1)
         }
         weekly.setOnClickListener {
-            task = GetData(listView, this, "weekly", status)
-            status.setText("Loading")
+            GetData.scope = "weekly"
+            task = GetData(listView, this, status)
+            status.text = res.getString(R.string.loading)
             task.execute(1)
         }
         monthly.setOnClickListener {
-            task = GetData(listView, this, "monthly", status)
-            status.setText("Loading")
+            GetData.scope = "monthly"
+            task = GetData(listView, this, status)
+            status.text = res.getString(R.string.loading)
             task.execute(1)
         }
-        listView.setOnItemClickListener { adapterView, view, i, l ->
-            onItemClick(adapterView, view, i, l, links)
+        listView.setOnItemClickListener { _, _, i, _ ->
+            onItemClick(i, links)
         }
         handler.post(this)
     }
-    private fun onItemClick(adpterView: AdapterView<*>, view: View, i: Int, l: Long, links: Array<String?>){
+    private fun onItemClick(
+        i: Int,
+        links: Array<String?>
+    ){
         if (links[i].isNullOrBlank()) return
-        var selectedLink = links[i]
-        var uri = Uri.parse(selectedLink)
-        var intent = Intent(Intent.ACTION_VIEW, uri)
+        val selectedLink = links[i]
+        val uri = Uri.parse(selectedLink)
+        val intent = Intent(Intent.ACTION_VIEW, uri)
         startActivity(intent)
     }
     override fun run(){
         links = task.linkArray.copyOf(30)
-        listView = findViewById<ListView>(R.id.listView)
-        listView.setOnItemClickListener { adapterView, view, i, l ->
-            onItemClick(adapterView, view, i, l, links)
+        listView = findViewById(R.id.listView)
+        listView.setOnItemClickListener { _, _, i, _ ->
+            onItemClick(i, links)
         }
         handler.postDelayed(this, 2000)
     }
